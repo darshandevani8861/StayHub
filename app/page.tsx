7ac8ddc3b4d9f36c8f7d85c7cad97203abdf6f45
Devani <darshandevani8861@gmail.com>
2024-04-21 14:06:44 +0530

    Final commit

--- app/page.tsx
+++ app/page.tsx
@@ -31,10 +31,10 @@ const Home = async ({searchParams}: HomeProps) => {
           grid
           grid-cols-1
           sm:grid-cols-2
-          md:grid-cols-3
-          lg:grid-cols-4
-          xl:grid-cols-5
-          2xl:grid-cols-6
+          md:grid-cols-2
+          lg:grid-cols-3
+          xl:grid-cols-4
+          2xl:grid-cols-5
           gap-8
         ">
           {listings.map((listing)=>{
